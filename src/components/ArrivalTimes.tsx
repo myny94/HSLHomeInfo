3425e955e4fb464070dfb74c067bf931c8c0b4b8
myny94 <myny94@gmail.com>
2023-02-13 15:18:40 +0200

    Add ArrivalTimes component
    - ArrivalTimes component displays bus times in flat list sorted by the arrival time
    - Earliest arrival bus from now would be displayed first

---
+++ src/components/ArrivalTimes.tsx
@@ -0,0 +1,48 @@
+import { GetStopsByRadiusQuery } from "../generated/graphql";
+import { RemainingTimeConverter, timeConverter } from "../util";
+import { Table } from "react-bootstrap";
+
+type ArrivalProps = {
+  arrivalQuery: GetStopsByRadiusQuery;
+};
+
+function ArrivalTimeDisplay(props: ArrivalProps) {
+  const arrivals =
+    props.arrivalQuery.stopsByRadius?.edges?.flatMap(
+      (stopData) => stopData?.node?.stop?.stoptimesWithoutPatterns ?? []
+    ) ?? [];
+  const sortedArrivals = arrivals.sort(
+    (a, b) => a?.scheduledArrival! >= b?.scheduledArrival! ? 1:-1
+  );
+
+  return (
+    <Table striped size="sm">
+      <thead>
+        <tr>
+          <th>Head sign</th>
+          <th>Scheduled arrival</th>
+          <th>Arrives in</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedArrivals.map((arrivalData) => (
+          <tr>
+            <td><img src="/images/busIcon.svg" alt="React Logo" width={20} height={20} />{arrivalData?.headsign}</td>
+            <td>
+              {timeConverter(
+                arrivalData?.serviceDay + arrivalData?.scheduledArrival
+              )}
+            </td>
+            <td>
+              {RemainingTimeConverter(
+                arrivalData?.serviceDay + arrivalData?.scheduledArrival
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </Table>
+  );
+}
+
+export default ArrivalTimeDisplay;
