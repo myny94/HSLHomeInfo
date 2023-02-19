1928faedf29527b9734d24651cf582630715db35
Nayeong Song <myny94@gmail.com>
2023-02-19 21:43:29 +0200

    Update coordinate logic changed
    - Update coordinate only if the user selects "my location" as an option
    - Default const parameters definition

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import {
   useGetStopByIdQuery,
   useGetStopsByRadiusQuery,
 } from "./generated/graphql";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Button } from "react-bootstrap";
 import ArrivalTimeDisplay from "./components/ArrivalTimes";
-import LocationAutocomplete from "./components/LocationAutocomplete";
-import { Coordinate } from "./types/Coordinate";
+import LocationAutocomplete, {
+  Option,
+} from "./components/LocationAutocomplete";
 import Form from "react-bootstrap/Form";
+import WarningIcon from "@mui/icons-material/Warning";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import DirectionsWalkIcon from "@mui/icons-material/DirectionsWalk";
+
+const DEFAULT_LATITUDE = 60.1705011;
+const DEFAULT_LONGITUDE = 24.941541;
+const DEFAULT_DISTANCE = 500;
+const DEFAULT_POLLINTERVAL = 60000;
 
 function App() {
   const [stopId, setStopId] = useState<string>("HSL:4200210");
-  const [latitude, setLatitude] = useState<number>(60.1705011);
-  const [longitude, setLongitude] = useState<number>(24.941541);
-  const [distance, setDistance] = useState<number>(1000);
+  const [latitude, setLatitude] = useState<number>(DEFAULT_LATITUDE);
+  const [longitude, setLongitude] = useState<number>(DEFAULT_LONGITUDE);
+  const [distance, setDistance] = useState<number>(DEFAULT_DISTANCE);
   const [status, setStatus] = useState<string | null>();
   const { data: stopData, loading: stopLoading } = useGetStopByIdQuery({
     variables: { stopId },
@@ -26,13 +34,8 @@ function App() {
       lon: longitude,
       radius: distance,
     },
-    pollInterval: 60000,
+    pollInterval: DEFAULT_POLLINTERVAL,
   });
-
-  // on mount
-  useEffect(() => {
-    getCurrentUserLocation();
-  }, [latitude, longitude]);
 
   const getCurrentUserLocation = () => {
     if (!navigator.geolocation) {
@@ -43,38 +46,53 @@ function App() {
         (position) => {
           setLatitude(position.coords.latitude);
           setLongitude(position.coords.longitude);
-          setStatus(`Current location is set to ${latitude},${longitude}`);
+          setStatus(null);
         },
         () => {
-          setStatus("Unable to retrieve your location.");
+          setStatus(
+            "Unable to retrieve your location. Make sure to allow location in your browser."
+          );
         }
       );
     }
   };
 
-  const UpdateCoordinate = async (coordinate: Coordinate) => {
-    setLatitude(coordinate.latitude);
-    setLongitude(coordinate.longitude);
+  const UpdateCoordinate = async (option: Option) => {
+    console.log(option);
+    if (option.type === "address") {
+      setLatitude(option.coordinate.latitude);
+      setLongitude(option.coordinate.longitude);
+      setStatus(null);
+    } else if (option.type === "mylocation") {
+      getCurrentUserLocation();
+    } else {
+      const _: never = option;
+    }
   };
 
   return (
     <div className="m-3">
-      <h3>HSL real time</h3>
-      <div className="rowDisplay">
-        <h4>{status}</h4>
-        <Button variant="secondary sm" onClick={getCurrentUserLocation}>
-          Set location again
-        </Button>
-      </div>
-      <LocationAutocomplete CoordinateCallback={UpdateCoordinate} />
-      <Form>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>Distance from the location (in meters)</Form.Label>
+      <h3>HSL real time table</h3>
+      <Form className="m-3">
+        <Form.Group controlId="formLocation">
+          <Form.Label>
+            <LocationOnIcon />
+            Location
+          </Form.Label>
+          <LocationAutocomplete CoordinateCallback={UpdateCoordinate} />
+          <Form.Text className="text-muted">{status}</Form.Text>
+        </Form.Group>
+        <Form.Group controlId="formDistance">
+          <Form.Label>
+            <DirectionsWalkIcon />
+            Distance from the location (in meters)
+          </Form.Label>
           <Form.Control
             type="number"
             placeholder="Enter distance limit you wish to set"
-            defaultValue={1000}
+            defaultValue={500}
             onBlur={(e) => setDistance(parseInt(e.target.value))}
+            onSubmit={(e) => alert("enter clicked")}
           />
           <Form.Text className="text-muted">
             Distances from the selected location to the bus stops
